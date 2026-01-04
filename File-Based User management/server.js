1a9526a15a2804e67a450eb003082d42cabf5785
ramiz181 <ramizmalik78685@gmail.com>
2026-01-04 23:08:02 +0500

    username field added

--- File-Based User management/server.js
+++ File-Based User management/server.js
@@ -10,25 +10,32 @@ app.use(express.urlencoded({ extended: false }))
 app.post('/auth/register/', (req, res) => {
     const body = req.body;
 
+    // id, firstname, lastname, email, password, 
+    // username ===> concat first last name + 3 digit random number at last
+
     const exists = users.some(user => user.email === body.email)
     console.log(exists);
+    let arr
 
     if (exists) {
         return res.json({ message: "user already exists" })
     }
     else {
-        users.push({ id: users.length + 1, ...body })
+        // Math.random()  ==> returns a number in the range: 0 <= n < 1
+        // adding 100 ==> cuz generated no. * 900 canbe less than 100 (2 digit)
+        const randomNumber = Math.floor(Math.random() * 900 + 100)
+        const username = body.firstName.toLowerCase() + body.lastName.toLowerCase() + randomNumber
+        console.log(username);
+
+        users.push({ id: users.length + 1, username, ...body })
         fs.writeFile('users.json', JSON.stringify(users), (err) => {
             if (err) throw err
         })
         res.json({ message: "User successfully registered" })
     }
 
-    console.log("this that these those");
 
 
-    // id, firstname, lastname, email, password, 
-    // username ===> concat first last name + 3 digit random number at last
     // const data = JSON.parse(fs.readFileSync('users.json', "utf-8"))
     // console.log(body);
 
